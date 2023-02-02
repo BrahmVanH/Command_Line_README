528e60a4af5bc7e9b549d037724901c5f8997db6
Brahm V <brahmvanh@gmail.com>
2023-02-01 19:31:54 -0500

    Got license rendering to work. Testing badges

---
+++ README.md
@@ -0,0 +1,37 @@
+# d
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+
+  ## Description 
+
+  s
+
+  ## Installation 
+
+  undefined
+
+  ## Usage
+
+  s
+
+  ## Credits 
+
+  s
+
+  ## License
+
+  (https://opensource.org/licenses/MIT)
+  
+
+  ## Features
+
+  f
+
+  ## Contributions
+
+  f
+
+  ## Test
+
+  undefined
+
--- utils/Licenses.js
+++ utils/Licenses.js
@@ -1,51 +1,53 @@
+
+
 const licenses = [
 {
-name: 'Apache 2.0',
+type: 'Apache 2.0',
 badge: '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]',
 link: '(https://opensource.org/licenses/Apache-2.0)',
 },
 {
-name: 'BSD 3-Clause',
+type: 'BSD 3-Clause',
 badge: '[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)]',
 link: '(https://opensource.org/licenses/BSD-3-Clause)',
 },
 {
-name: 'BSD 2-Clause',
+type: 'BSD 2-Clause',
 badge: '[![License](https://img.shields.io/badge/License-BSD_2--Clause-orange.svg)]',
 link: '(https://opensource.org/licenses/BSD-2-Clause)',
 },
 {
-name: 'GPL v3',
+type: 'GPL v3',
 badge: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]',
 link: '(https://www.gnu.org/licenses/gpl-3.0)',
 },
 {
-name: 'GPL v2',
+type: 'GPL v2',
 badge: '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)]',
 link: '(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)',
 },
 {
-name: 'AGPL v3',
+type: 'AGPL v3',
 badge: '[![License: AGPL v3](https://img.shields.io/badge/License-AGPL_v3-blue.svg)]',
 link: '(https://www.gnu.org/licenses/agpl-3.0)',
 },
 {
-name: 'LGPL v3',
+type: 'LGPL v3',
 badge: '[![License: LGPL v3](https://img.shields.io/badge/License-LGPL_v3-blue.svg)]',
 link: '(https://www.gnu.org/licenses/lgpl-3.0)',
 },
 {
-name: 'Unlicense',
+type: 'Unlicense',
 badge: '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)]',
 link: '(http://unlicense.org/)',
 },
 {
-name: 'The Do What the Fuck You Want to Public License',
+type: 'The Do What the Fuck You Want to Public License',
 badge: '[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)]',
 link: '(http://www.wtfpl.net/about/)',
 },
 {
-name: 'MIT',
+type: 'MIT',
 badge: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]',
 link: '(https://opensource.org/licenses/MIT)',
 },
